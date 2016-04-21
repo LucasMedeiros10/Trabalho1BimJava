918d9e8690afc1118dcbd4ddd0336925667ef262
Lucas Medeiros <lucas_medeiros09@hotmail.com>
2016-04-20 23:21:24 -0300

    Realizado implementação dos atributos, gets e setters dos atributos e criação do construtor na classe Cliente;

--- src/br/univel/classes/Cliente.java
+++ src/br/univel/classes/Cliente.java
@@ -1,5 +1,47 @@
 package br.univel.classes;
 
+import br.univel.enums.EstadoCivil;
+
 public class Cliente {
 
+	private int id;
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private EstadoCivil estadoCivil;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
+	public Cliente(){
+		
+	}
 }
